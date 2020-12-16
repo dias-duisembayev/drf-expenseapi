e74f825fe5e6b0291c42f278a6eeb1fa23c8519b
dias_duisembayev <dias.duisembayev@gmail.com>
2020-12-16 12:36:24 +0600

    Category and Expense models created

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    definition = models.TextField(blank=True, default='')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('name', 'owner',)
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=50)
+    definition = models.TextField(blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    created_at = models.DateField()
+
+    class Meta:
+        unique_together = ('name', 'category',)
+
+    def __str__(self):
+        return self.name
