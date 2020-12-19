2e763f95047d297e19a41d9bda742cfd92a8af71
dias_duisembayev <dias.duisembayev@gmail.com>
2020-12-19 18:16:24 +0600

    fixed URL for getting all expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -6,7 +6,7 @@ from .views import CategoryCreation, CategoryList, CategoryDetail, \
 urlpatterns = [
     path('categories/', CategoryList.as_view()),
     path('categories/create/', CategoryCreation.as_view()),
-    path('categories/expenses/all', ExpenseListAll.as_view()),
+    path('categories/expenses/all/', ExpenseListAll.as_view()),
     re_path('^categories/(?P<category_id>.+)/expenses/create/$', ExpenseCreation.as_view()),
     re_path('^categories/(?P<category_id>.+)/expenses/(?P<expense_id>.+)/$', ExpenseDetail.as_view()),
     re_path('^categories/(?P<category_id>.+)/expenses/$', ExpenseListByCategory.as_view()),
