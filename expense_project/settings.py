311cb75f879a394ddf9ae03e5b4a3673903ccfaa
dias_duisembayev <dias.duisembayev@gmail.com>
2020-12-20 13:03:35 +0600

    django-cors-headers added

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'rest_auth',
     'rest_auth.registration',
+    'corsheaders',
     # Local apps
     'expenses.apps.ExpensesConfig',
 ]
@@ -69,13 +70,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
 ROOT_URLCONF = 'expense_project.urls'
 
 TEMPLATES = [
